79c31487c0df77935ef4a9e9f927807355d723df
ritsu <ritsu@users.noreply.github.com>
2017-09-14 06:08:52 -0400

    Add 167, 168

---
+++ java/src/net/qiguang/leetcode/L167_TwoSumInputArrayIsSorted.java
@@ -0,0 +1,63 @@
+package net.qiguang.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Given an array of integers that is already sorted in ascending order,
+ * find two numbers such that they add up to a specific target number.
+ *
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1
+ * must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+ *
+ * You may assume that each input would have exactly one solution and you may not use the same element twice.
+ *
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ */
+
+public class L167_TwoSumInputArrayIsSorted {
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if      (numbers[left] + numbers[right] > target) right--;
+            else if (numbers[left] + numbers[right] < target) left++;
+            else return new int[] {left + 1, right + 1};
+        }
+        return null;
+    }
+    public static int[] twoSumHashMap(int[] numbers, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int key = target - numbers[i];
+            if (hm.containsKey(key))
+                return new int[] {hm.get(key) + 1, i + 1};
+            hm.put(numbers[i], i);
+        }
+        return null;
+    }
+    public static int[] twoSumBinarySearch(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            int key = target - numbers[i];
+            int lo = i + 1;
+            int hi = numbers.length - 1;
+            while (hi >= lo) {
+                int mid = lo + (hi - lo) / 2;
+                if      (numbers[mid] < key) lo = mid + 1;
+                else if (numbers[mid] > key) hi = mid - 1;
+                else return new int[] {i+1, mid+1};
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        int[] a = new int[100];
+        for (int i = 0; i < 100; i++) {
+            a[i] = i;
+        }
+        for (int i = 197; i > 187; i--) {
+            System.out.println(Arrays.toString(twoSum(a, i)));
+        }
+    }
+}
---
+++ java/src/net/qiguang/leetcode/L168_ExcelSheetColumnTitle.java
@@ -0,0 +1,33 @@
+package net.qiguang.leetcode;
+
+/**
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ *
+ * For example:
+ *   1 -> A
+ *   2 -> B
+ *   3 -> C
+ *   ...
+ *   26 -> Z
+ *   27 -> AA
+ *   28 -> AB
+ */
+public class L168_ExcelSheetColumnTitle {
+    public static String convertToTitle(int n) {
+        StringBuilder s = new StringBuilder();
+        while (n > 0) {
+            s.append((char) ('A' + (n - 1) % 26));
+            n = (n - 1) / 26;
+        }
+        return s.reverse().toString();
+    }
+    public static String convertToTitleRecursive(int n) {
+        if (n == 0) return "";
+        return convertToTitleRecursive((n - 1) / 26) + (char) ('A' + (n - 1) % 26);
+    }
+    public static void main(String[] args) {
+        for (int i = 1; i < 1000; i++) {
+            System.out.println(convertToTitle(i));
+        }
+    }
+}
