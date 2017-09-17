adb53cc7563d4a3683e42c1516907e8a3bfef66b
ritsu <ritsu@users.noreply.github.com>
2017-09-17 04:37:49 -0400

    Add 202, 203

---
+++ java/src/net/qiguang/leetcode/L202_HappyNumber.java
@@ -0,0 +1,59 @@
+package net.qiguang.leetcode;
+
+import java.util.HashSet;
+
+/**
+ * Write an algorithm to determine if a number is "happy".
+ *
+ * A happy number is a number defined by the following process:
+ * Starting with any positive integer, replace the number by the sum of the squares of its digits,
+ * and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in
+ * a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+ *
+ * Example: 19 is a happy number
+ *   1^2 + 9^2 = 82
+ *   8^2 + 2^2 = 68
+ *   6^2 + 8^2 = 100
+ *   1^2 + 0^2 + 0^2 = 1
+ */
+public class L202_HappyNumber {
+    public boolean isHappy(int n) {
+        int slow = happySum(n);
+        int fast = happySum(slow);
+        while (slow != fast) {
+            slow = happySum(slow);
+            fast = happySum(fast);
+            fast = happySum(fast);
+        }
+        return slow == 1;
+    }
+    public int happySum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += Math.pow(n % 10, 2);
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public boolean isHappyHash(int n) {
+        HashSet<Integer> h = new HashSet<>();
+        while (h.contains(n) == false) {
+            h.add(n);
+            int sum = 0;
+            while (n > 0) {
+                sum += Math.pow(n % 10, 2);
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        L202_HappyNumber lc = new L202_HappyNumber();
+        for (int i = 0; i < 1000; i++) {
+            System.out.printf("%5d %s\n", i, lc.isHappy(i));
+        }
+    }
+}
---
+++ java/src/net/qiguang/leetcode/L203_RemoveLinkedListElements.java
@@ -0,0 +1,63 @@
+package net.qiguang.leetcode;
+
+import java.util.Random;
+
+/**
+ * Remove all elements from a linked list of integers that have value val.
+ *
+ * Example
+ * Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+ * Return: 1 --> 2 --> 3 --> 4 --> 5
+ */
+class L203_RemoveLinkedListElements {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+        public String toString() {
+            ListNode current = this;
+            StringBuilder sb = new StringBuilder();
+            while (current != null) {
+                sb.append(current.val + " ");
+                current = current.next;
+            }
+            return sb.toString();
+        }
+    }
+    public static ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        if (head.val == val) return removeElements(head.next, val);
+
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            while (next != null && next.val == val) {
+                next = next.next;
+            }
+            current.next = next;
+            current = next;
+        }
+        return head;
+    }
+    public static void main(String[] args) {
+        int listsize = 20;
+        int maxval = 10;
+
+        // Create list
+        ListNode head = new ListNode(0);
+        ListNode current = head;
+        Random r = new Random();
+        for (int i = 1; i < listsize; i++) {
+            current.next = new ListNode(r.nextInt(maxval));
+            current = current.next;
+        }
+        System.out.printf("%4s %s\n", "", head);
+
+        // Remove random items
+        while (head != null) {
+            int val = r.nextInt(maxval);
+            head = removeElements(head, val);
+            System.out.printf("[%2d] %s\n", val, head);
+        }
+    }
+}
