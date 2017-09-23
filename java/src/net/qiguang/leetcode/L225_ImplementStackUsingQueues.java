ceb622181b503380d19feb275f7b4410498b1616
Ritsu <ritsu@users.noreply.github.com>
2017-09-23 17:10:10 -0400

    Add additional solution

--- java/src/net/qiguang/leetcode/L225_ImplementStackUsingQueues.java
+++ java/src/net/qiguang/leetcode/L225_ImplementStackUsingQueues.java
@@ -27,43 +27,45 @@ import java.util.Queue;
  *
  *   You may assume that all operations are valid (for example, no pop or top operations will be called
  *   on an empty stack).
+ *
+ * Note: Running time varied wildly for multiple submissions of the exact same code,
+ *       anywhere from 96% to 6% for both implementations.
  */
 public class L225_ImplementStackUsingQueues {
-    // 88 ms ~91%
+    // 86 ms ~96%, O(1)
     public class MyStack {
-        ArrayDeque<ArrayDeque> first;
+        ArrayDeque<ArrayDeque> queue;
 
         /** Initialize your data structure here. */
         public MyStack() {
-            first = null;
+            queue = null;
         }
 
         /** Push element x onto stack. */
         public void push(int x) {
-            ArrayDeque<ArrayDeque> oldfirst = first;
-            first = new ArrayDeque<ArrayDeque>();
-            ArrayDeque<Integer> firstval = new ArrayDeque<Integer>();
-            firstval.add(x);
-            first.add(firstval);
-            if (oldfirst != null) first.add(oldfirst);
+            ArrayDeque<ArrayDeque> old = queue;
+            queue = new ArrayDeque<>();
+            ArrayDeque<Integer> val = new ArrayDeque<>();
+            val.add(x);
+            queue.add(val);
+            if (old != null) queue.add(old);
         }
 
         /** Removes the element on top of the stack and returns that element. */
         public int pop() {
-            int val = (int) first.poll().poll();
-            first = first.poll();
+            int val = (int) queue.poll().poll();
+            queue = queue.poll();
             return val;
         }
 
         /** Get the top element. */
         public int top() {
-            ArrayDeque<Integer> tmp = first.peek();
-            return tmp.peek();
+            return (int) queue.peek().peek();
         }
 
         /** Returns whether the stack is empty. */
         public boolean empty() {
-            return first == null;
+            return queue == null;
         }
     }
 
@@ -71,9 +73,46 @@ public class L225_ImplementStackUsingQueues {
         return new MyStack();
     }
 
+    // 87 ms, 94% O(N) for push
+    public class MyStack2 {
+        ArrayDeque<Integer> queue;
+
+        /** Initialize your data structure here. */
+        public MyStack2() {
+            queue = new ArrayDeque<>();
+        }
+
+        /** Push element x onto stack. */
+        public void push(int x) {
+            int s = queue.size();
+            queue.add(x);
+            while (s-- > 0)
+                queue.add(queue.poll());
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int pop() {
+            return queue.poll();
+        }
+
+        /** Get the top element. */
+        public int top() {
+            return queue.peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public boolean empty() {
+            return queue.isEmpty();
+        }
+    }
+
+    public MyStack2 getStack2() {
+        return new MyStack2();
+    }
+
     public static void main(String[] args) {
         L225_ImplementStackUsingQueues lc = new L225_ImplementStackUsingQueues();
-        MyStack s = lc.getStack();
+        MyStack2 s = lc.getStack2();
         for (int i = 0; i < 10; i++) {
             s.push(i);
         }
